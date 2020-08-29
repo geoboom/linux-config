ed4c193f8a34d2ee43657ae7afd3e9aaf60f2578
geoboom <leegeorgie52@gmail.com>
2020-08-29 11:05:32 +0800

    add zshrc config

--- repo_to_system.sh
+++ repo_to_system.sh
@@ -2,8 +2,10 @@ rm -rf ~/.config/nvim/
 rm -rf ~/.i3/
 rm ~/.tmux.conf
 rm ~/.Xresources
+rm ~/.zshrc
 
 cp -r ./nvim/ ~/.config/nvim/
 cp -r ./.i3/ ~/.i3/
 cp ./.tmux.conf ~
 cp ./.Xresources ~
+cp ./.zshrc ~
--- system_to_repo.sh
+++ system_to_repo.sh
@@ -2,8 +2,10 @@ rm -rf ./nvim/
 rm -rf ./.i3/
 rm ./.tmux.conf
 rm ./.Xresources
+rm ./.zshrc
 
 cp -r ~/.config/nvim/ .
 cp -r ~/.i3/ .
 cp ~/.tmux.conf .
 cp ~/.Xresources .
+cp ~/.zshrc .
