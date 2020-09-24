ee0a7e39958c5f162a230fb9974555e66da8f253
geoboom <leegeorgie52@gmail.com>
2020-09-24 19:10:19 +0800

    Add copy alacritty config

--- WARNING_repo_to_system.sh
+++ WARNING_repo_to_system.sh
@@ -3,9 +3,11 @@ rm -rf ~/.i3/
 rm ~/.tmux.conf
 rm ~/.Xresources
 rm ~/.zshrc
+rm -rf ~/.config/alacritty/
 
 cp -r ./nvim/ ~/.config/nvim/
 cp -r ./.i3/ ~/.i3/
 cp ./.tmux.conf ~
 cp ./.Xresources ~
 cp ./.zshrc ~
+cp -r ./alacritty/ ~/.config/alacritty/
