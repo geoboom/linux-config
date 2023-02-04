0d8f37a262b644e21127d16581b25d0c65c7acac
geoboom <leegeorgie52@gmail.com>
2023-02-04 22:07:20 +0800

    update buf format to new api

--- configuration-files/.config/nvim/lua/lsp_config.lua
+++ configuration-files/.config/nvim/lua/lsp_config.lua
@@ -17,7 +17,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
 
   buf_set_keymap('n', '<BSLASH>cl', '<cmd>lua vim.lsp.codelens.refresh()<CR>', opts)
   buf_set_keymap('n', '<BSLASH>cll', '<cmd>lua vim.lsp.codelens.run()<CR>', opts)
